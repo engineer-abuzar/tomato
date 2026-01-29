1511426c8817339e5e1f0393616a9f903328d76a
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2026-01-29 11:12:05 +0530

    Admin and frontend adds env var

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = process.env.api_address
   const [token, setToken] = useState("");
 
   //remove food_list state 
