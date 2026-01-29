8d69b1bbb87f9e0d184924e54b02f861cc51c0a9
engineer-abuzar <engineer.abuzarahmed@gmail.com>
2026-01-29 10:32:47 +0530

    Deployment setting adds

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb://localhost:27017').then(()=>{
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>{
        console.log('DB connected') ;
     })
 }
